56c1325d12eef194420f2330b092bfc1d974294c
Colin Tück <colin.tueck@eqar.eu>
2022-09-24 02:00:52 +0200

    [Kerberos] enhance config & loading of data

--- entrypoint.kadmin.sh
+++ entrypoint.kadmin.sh
@@ -12,6 +12,11 @@ generate_config
 # initialize database if needed
 if [ -f /var/lib/krb5kdc/principal ]; then
     echo "Database present."
+elif [ -d /initdb.d ] && [ -f /initdb.d/krb5_stash ] && [ -f /initdb.d/krb5_dump ]; then
+    echo -n "Initializing Kerberos database from /initdb.d: "
+    cp /initdb.d/krb5_stash /var/lib/krb5kdc/stash
+    kdb5_util load -verbose /initdb.d/krb5_dump
+    echo "Done."
 else
     echo "No database present, initializing:"
     if [ -z "${KRB_MASTER_PASSWORD}" ]; then
@@ -22,8 +27,10 @@ else
     kdb5_util -r ${KRB_REALM} -P "${KRB_MASTER_PASSWORD}" create -s && echo "OK." || echo "Failed."
     if [ -z "${KRB_ADMIN_PASSWORD}" ]; then
         KRB_ADMIN_PASSWORD=$(rand_password)
-        echo "- random password for admin principal: ${KRB_ADMIN_PASSWORD}"
+        echo "- random password for admin principals: ${KRB_ADMIN_PASSWORD}"
     fi
+    echo -n "- creating principal ${KRB_DEFAULT_ADMIN}/admin@${KRB_REALM}: "
+    kadmin.local addprinc -pw "${KRB_ADMIN_PASSWORD}" "${KRB_DEFAULT_ADMIN}/admin" && echo "OK." || echo "Failed."
     echo -n "- creating principal ${KRB_DEFAULT_ADMIN}@${KRB_REALM}: "
     kadmin.local addprinc -pw "${KRB_ADMIN_PASSWORD}" "${KRB_DEFAULT_ADMIN}" && echo "OK." || echo "Failed."
 fi
@@ -31,8 +38,8 @@ fi
 echo "Checking for required principals and keytabs:"
 # check if principals exist & generate keytabs
 check_principal_keytab host/saslauthd._docker saslauthd.keytab
-check_principal_keytab ldap/${LDAP_HOSTNAME:-slapd._docker} slapd.keytab
-check_principal_keytab ldapcherry ldapcherry.keytab
+check_principal_keytab ldap/${LDAP_FQDN} slapd.keytab
+check_principal_keytab ldapcherry/ldapcherry._docker ldapcherry.keytab
 
 exec "$@"
 
