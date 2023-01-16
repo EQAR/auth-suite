94d1cbb88c35f2fef0323dc4d2929373ca66a4fc
Colin Tück <colin.tueck@eqar.eu>
2023-01-16 21:58:29 +0100

    ldapcherry: fetch from GitHub, improve config

--- build/entrypoint.ldapcherry.sh
+++ build/entrypoint.ldapcherry.sh
@@ -5,7 +5,19 @@
 # generate necessary config files
 check_if_realm_in_env
 check_if_suffix_in_env
-generate_config '$LDAP_SUFFIX $KRB_REALM $LDAPCHERRY_URL $LDAPCHERRY_MAILFROM'
+
+# set default values
+set -a
+if [ -z "${LDAPCHERRY_PARENT_DN}" ]; then
+    # create default parent DN list
+    LDAPCHERRY_PARENT_DN="[ '${LDAP_SUFFIX}' ]"
+fi
+if [ -z "${LDAPCHERRY_ADMIN_GROUP_DN}" ]; then
+    # set default admin group DN
+    LDAPCHERRY_ADMIN_GROUP_DN="cn=admin,${LDAP_SUFFIX}"
+fi
+
+generate_config '$LDAP_SUFFIX $KRB_REALM $LDAPCHERRY_URL $LDAPCHERRY_MAILFROM $LDAPCHERRY_PARENT_DN $LDAPCHERRY_ADMIN_GROUP_DN'
 
 exec "$@"
 
